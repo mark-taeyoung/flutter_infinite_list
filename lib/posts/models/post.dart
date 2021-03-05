a9defdcebcc263784c9c449d55aa0ef234916351
mark-taeyoung <mark.taeyoung@icloud.com>
2021-03-05 13:48:52 +0900

    code done

--- lib/posts/models/post.dart
+++ lib/posts/models/post.dart
@@ -1,11 +1,11 @@
 import 'package:equatable/equatable.dart';
 
 class Post extends Equatable {
+  const Post({this.id, this.title, this.body});
+
   final int id;
   final String title;
   final String body;
-
-  const Post({this.id, this.title, this.body});
 
   @override
   List<Object> get props => [id, title, body];
